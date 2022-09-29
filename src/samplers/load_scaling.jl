f32832609fb8ddcd4ca7295880d8bd54dc8dc8d4
hwallace30 <hwallace30@gatech.edu>
2022-09-28 21:13:16 -0400

    new SLS method that takes in array (l, u) tuples

--- src/samplers/load_scaling.jl
+++ src/samplers/load_scaling.jl
@@ -18,52 +18,79 @@ where ``\\alpha_{i}``s are sampled i.i.d from \\sim U[l, u]``, and ``\\bar{p}^{d
     are the reference active and reactive demands.
 """
 struct SimpleLoadScaling <: AbstractLoadSampler
-    d::Uniform{Float64}
-    d_scl::Uniform{Float64}
+    # d::Uniform{Float64} # Q about generalizing this feature, as well as having r take in only [0,1]
+    r::Matrix{Float64}
 
     # Reference active/reactive power demand
     pd_ref::Vector{Float64}
     qd_ref::Vector{Float64}
 
     # constructor
-    function SimpleLoadScaling(pd::Vector{Float64}, qd::Vector{Float64}, l::Float64, 
-                               u::Float64, l_scl::Float64, u_scl::Float64)
+    function SimpleLoadScaling(pd::Vector{Float64}, qd::Vector{Float64}, l::Float64, u::Float64)
         L = length(pd)
         length(qd) == L || error("pd and qd must have same size")
 
-        return new(Uniform(l, u), Uniform(l_scl, u_scl), copy(pd), copy(qd))
+        # return new(Uniform(l, u), copy(pd), copy(qd))
+        return new(hcat(fill(l, L), fill(u, L))', copy(pd), copy(qd))
     end
+    
+    function SimpleLoadScaling(pd::Vector{Float64}, qd::Vector{Float64}, a::Vector{Tuple{Float64, Float64}})
+        L = length(pd)
+        length(qd) == L || error("pd and qd must have same size")
+
+        a_mtx = hcat(collect.(a)...)
+        length(a_mtx[1, :]) == L || error("'a' must be the same size as pd and qd")
+
+        all(a_mtx[1, :] .<= a_mtx[2, :]) || error("for all tuples (l, u), l <= u.")
+
+        return new(a_mtx, copy(pd), copy(qd))
+    end
+    
 end
 
-function SimpleLoadScaling(data::Dict, l::Float64, u::Float64, l_scl::Float64, u_scl::Float64)
+function SimpleLoadScaling(data::Dict, l::Float64, u::Float64)
     L = length(data["load"])
     pd = zeros(Float64, L)
     qd = zeros(Float64, L)
-    for i in 1:L # vectorizable
+    for i in 1:L
         ldata = data["load"]["$i"]
         pd[i] = ldata["pd"]
         qd[i] = ldata["qd"]
     end
-    return SimpleLoadScaling(pd, qd, l, u, l_scl, u_scl)
+    return SimpleLoadScaling(pd, qd, l, u)
+end
+
+function SimpleLoadScaling(data::Dict, a::Vector{Tuple{Float64, Float64}})
+    L = length(data["load"])
+    pd = zeros(Float64, L)
+    qd = zeros(Float64, L)
+    for i in 1:L 
+        ldata = data["load"]["$i"]
+        pd[i] = ldata["pd"]
+        qd[i] = ldata["qd"]
+    end
+    return SimpleLoadScaling(pd, qd, a)
 end
 
 function _sample_loads(rng, ls::SimpleLoadScaling)
-    # Sample multiplicative noise
+    # Sample multiplicative noise and re-scale demand
+    l = ls.r[1, :]
+    u = ls.r[2, :]
     L = length(ls.pd_ref)
-    α = rand(rng, ls.d_scl, L)
-    l = minimum(ls.d)
-    u = maximum(ls.d)
-    β = Vector{Float64}(undef, L)
-    
-    # samples from L unique Uniform distributions
+    # scaling vector
+    α = Vector{Float64}(undef, L)
+    # distribution vector
+    # d_pd = Vector{Uniform{Float64}}(undef, L)
+    # d_qd = Vector{Uniform{Float64}}(undef, L)
+      
     for i in 1:L
-        scale = α[i]
-        β[i] = rand(rng, Uniform(l*scale, u*scale) ) # vectorizable
+        dist = Uniform(l[i], u[i])
+        α[i] = rand(rng, dist)
+        # d_pd[i] = dist*ls.pd_ref[i]
+        # d_qd[i] = dist*ls.qd_ref[i]
     end
-    
-    # apply weights to reference loads
-    pd = ls.pd_ref .* β
-    qd = ls.qd_ref .* β
+    pd = ls.pd_ref .* α
+    qd = ls.qd_ref .* α
 
-    return pd, qd
-end 
+    return pd, qd#, d_pd, d_qd
+end
