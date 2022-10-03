39c4c5537d52a0697ad3b29850b2c889cddde0af
mtanneau3 <mtanneau3@gatech.edu>
2022-10-03 15:57:45 +0000

    Update sampler code

--- src/samplers/load_scaling.jl
+++ src/samplers/load_scaling.jl
@@ -5,41 +5,39 @@ Load sampler that re-scales all loads by a multiplicative noise.
 
 ```math
 \\begin{lmatrix}
-    pd_{i}\\
-    qd_{i}
+    p^{d}_{i}\\
+    q^{d}_{i}
 \\end{lmatrix}
 =
 \\begin{lmatrix}
-    \\alpha_{i} \\times \\bar{pd}_{i}\\
-    \\alpha_{i} \\times \\bar{qd}_{i}
+    \\alpha_{i} \\times \\bar{p}^{d}_{i}\\\\
+    \\alpha_{i} \\times \\bar{q}^{d}_{i}
 \\end{lmatrix}
 ```
-where ``\\alpha_{i}``s are sampled i.i.d from \\sim U[l, u]``, and ``\\bar{pd}, \\bar{qd}``
-    are the reference active and reactive demands.
+where ``\\bar{p}^{d}, \\bar{q}^{d}`` are the reference active and reactive demands,
+    and ``\\alpha_{i} \\sim U[l_{i}, u_{i}]``. The ``\\alpha_{i}``s are sampled i.i.d, i.e.,
+    the noise is uncorrelated between loads.
 """
 struct SimpleLoadScaling <: AbstractLoadSampler
-    # Upper and lower Uniform bounds, size (2, L) where L = number of loads
-    r::Matrix{Float64}
+    # Lower & upper bounds of multiplicative noise
+    l::Vector{Float64}
+    u::Vector{Float64}
 
     # Reference active/reactive power demand
     pd_ref::Vector{Float64}
     qd_ref::Vector{Float64}
 
-    # constructor
-    function SimpleLoadScaling(pd::Vector{Float64}, qd::Vector{Float64}, l::Float64, u::Float64)
+    # Constructor
+    function SimpleLoadScaling(l::Vector{Float64}, u::Vector{Float64}, pd::Vector{Float64}, qd::Vector{Float64})
         L = length(pd)
         length(qd) == L || error("pd and qd must have same size")
+        length(l) == L || error("Lower bounds must be the same size as pd and qd")
+        length(u) == L || error("Upper bounds must be the same size as pd and qd")
 
-        return new(hcat(fill(l, L), fill(u, L))', copy(pd), copy(qd))
-    end
-    
-    function SimpleLoadScaling(pd::Vector{Float64}, qd::Vector{Float64}, r::Matrix{Float64})
-        L = length(pd)
-        length(qd) == L || error("pd and qd must have same size")
-        length(r[1, :]) == L || error("'r' must be the same size as pd and qd")
-        all(r[1, :] .< r[2, :]) || error("for all bounds (l, u), l must be less than u.")
+        all(l .<= u) || error("for all bounds (l, u), `l` must be smaller than or equal to `u`.")
 
-        return new(r, copy(pd), copy(qd))
+        # Copy original data to avoid external changes afterwards
+        return new(copy(l), copy(u), copy(pd), copy(qd))
     end
     
 end
@@ -53,33 +51,29 @@ function SimpleLoadScaling(data::Dict, l::Float64, u::Float64)
         pd[i] = ldata["pd"]
         qd[i] = ldata["qd"]
     end
-    return SimpleLoadScaling(pd, qd, l, u)
+    return SimpleLoadScaling(l .* ones(Float64, L), u .* ones(Float64, L), pd, qd)
 end
 
-function SimpleLoadScaling(data::Dict, r::Matrix{Float64})
+function SimpleLoadScaling(data::Dict, l::Vector{Float64}, u::Vector{Float64})
     L = length(data["load"])
     pd = zeros(Float64, L)
     qd = zeros(Float64, L)
-    for i in 1:L 
+    for i in 1:L
         ldata = data["load"]["$i"]
         pd[i] = ldata["pd"]
         qd[i] = ldata["qd"]
     end
-    return SimpleLoadScaling(pd, qd, r)
+    return SimpleLoadScaling(l, u, pd, qd)
 end
 
 function _sample_loads(rng, ls::SimpleLoadScaling)
-    # Sample multiplicative noise and re-scale demand
-    l = ls.r[1, :]
-    u = ls.r[2, :]
     L = length(ls.pd_ref)
-    # scaling vector
-    α = Vector{Float64}(undef, L)
-
-    for i in 1:L
-        dist = Uniform(l[i], u[i])
-        α[i] = rand(rng, dist)
-    end
+    
+    # Sample multiplicative noise
+    d = Uniform(0.0, 1.0)
+    α = rand(rng, d, L)
+    α .*= (ls.u .- ls.l)
+    α .+= ls.l  # this gives us α[i] ~ U(l[i], u[i])
 
     pd = ls.pd_ref .* α
     qd = ls.qd_ref .* α
