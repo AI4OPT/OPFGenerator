ce6f9eeea8beb2662cce822adf09e0a6b46d5a05
henwallace007@gmail.com <hwallace30@gatech.edu>
2022-11-13 17:50:07 -0500

    ac_opf solution dictionary

---
+++ src/acopf.jl
@@ -0,0 +1,200 @@
+"""
+    build_acopf(data, optimizer)
+Build an AC-OPF model.
+This implementation is based on the AC-OPF formulation of Rosetta-OPF
+    https://github.com/lanl-ansi/rosetta-opf/blob/38a951326df3156d79dcdc49c8010aa29905b05d/jump.jl
+"""
+function build_acopf(data::Dict{String,Any}, optimizer)
+    # Cleanup and pre-process data
+    PM.standardize_cost_terms!(data, order=2)
+    PM.calc_thermal_limits!(data)
+    ref = PM.build_ref(data)[:it][:pm][:nw][0]
+
+    # Check that slack bus is unique
+    length(ref[:ref_buses]) == 1 || error("Data has $(length(ref[:ref_buses])) slack buses (expected 1).")
+    i0 = first(keys(ref[:ref_buses]))
+    
+    # Grab some data
+    N = length(ref[:bus])
+    G = length(ref[:gen])
+    E = length(ref[:branch])
+    L = length(ref[:load])
+    bus_loads = [
+        [ref[:load][l] for l in ref[:bus_loads][i]]
+        for i in 1:N
+    ]
+    bus_shunts = [
+        [ref[:shunt][s] for s in ref[:bus_shunts][i]]
+        for i in 1:N
+    ]
+
+    model = JuMP.Model(optimizer)
+
+    #
+    #   I. Variables
+    #
+    # nodal voltage
+    JuMP.@variable(model, va[1:N])
+    JuMP.@variable(model, ref[:bus][i]["vmin"] <= vm[i in 1:N] <= ref[:bus][i]["vmax"], start=1.0)
+    # Active and reactive dispatch
+    JuMP.@variable(model, ref[:gen][g]["pmin"] <= pg[g in 1:G] <= ref[:gen][g]["pmax"])
+    JuMP.@variable(model, ref[:gen][g]["qmin"] <= qg[g in 1:G] <= ref[:gen][g]["qmax"])
+    # Bi-directional branch flows
+    JuMP.@variable(model, -ref[:branch][l]["rate_a"] <= pf[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+    JuMP.@variable(model, -ref[:branch][l]["rate_a"] <= qf[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+
+    # 
+    #   II. Constraints
+    #
+    # Slack bus
+    JuMP.@constraint(model, slack_bus, va[i0] == 0.0)
+
+    # Nodal power balance
+    JuMP.@constraint(model, 
+        kirchhoff_active[i in 1:N],
+        sum(pf[a] for a in ref[:bus_arcs][i]) ==
+        sum(pg[g] for g in ref[:bus_gens][i]) -
+        sum(load["pd"] for load in bus_loads[i]) -
+        sum(shunt["gs"] for shunt in bus_shunts[i])*vm[i]^2
+    )
+    JuMP.@constraint(model,
+        kirchhoff_reactive[i in 1:N],
+        sum(qf[a] for a in ref[:bus_arcs][i]) ==
+        sum(qg[g] for g in ref[:bus_gens][i]) -
+        sum(load["qd"] for load in bus_loads[i]) +
+        sum(shunt["bs"] for shunt in bus_shunts[i])*vm[i]^2
+    )
+
+    # Branch power flow physics and limit constraints
+    # We pre-allocate constraint containers for simplicity
+    model[:thermal_limit_fr] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:thermal_limit_to] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:voltage_difference_limit] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_active_fr] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_active_to] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_reactive_fr] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_reactive_to] = Vector{JuMP.ConstraintRef}(undef, E)
+    for (i,branch) in ref[:branch]
+        f_idx = (i, branch["f_bus"], branch["t_bus"])
+        t_idx = (i, branch["t_bus"], branch["f_bus"])
+
+        p_fr = pf[f_idx]
+        q_fr = qf[f_idx]
+        p_to = pf[t_idx]
+        q_to = qf[t_idx]
+
+        vm_fr = vm[branch["f_bus"]]
+        vm_to = vm[branch["t_bus"]]
+        va_fr = va[branch["f_bus"]]
+        va_to = va[branch["t_bus"]]
+
+        g, b = PM.calc_branch_y(branch)
+        tr, ti = PM.calc_branch_t(branch)
+        ttm = tr^2 + ti^2
+        g_fr = branch["g_fr"]
+        b_fr = branch["b_fr"]
+        g_to = branch["g_to"]
+        b_to = branch["b_to"]
+
+        # From side of the branch flow
+        model[:ohm_active_fr][i] = JuMP.@NLconstraint(model, p_fr ==  (g+g_fr)/ttm*vm_fr^2 + (-g*tr+b*ti)/ttm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-b*tr-g*ti)/ttm*(vm_fr*vm_to*sin(va_fr-va_to)) )
+        model[:ohm_reactive_fr][i] = JuMP.@NLconstraint(model, q_fr == -(b+b_fr)/ttm*vm_fr^2 - (-b*tr-g*ti)/ttm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-g*tr+b*ti)/ttm*(vm_fr*vm_to*sin(va_fr-va_to)) )
+
+        # To side of the branch flow
+        model[:ohm_active_to][i] = JuMP.@NLconstraint(model, p_to ==  (g+g_to)*vm_to^2 + (-g*tr-b*ti)/ttm*(vm_to*vm_fr*cos(va_to-va_fr)) + (-b*tr+g*ti)/ttm*(vm_to*vm_fr*sin(va_to-va_fr)) )
+        model[:ohm_reactive_to][i] = JuMP.@NLconstraint(model, q_to == -(b+b_to)*vm_to^2 - (-b*tr+g*ti)/ttm*(vm_to*vm_fr*cos(va_to-va_fr)) + (-g*tr-b*ti)/ttm*(vm_to*vm_fr*sin(va_to-va_fr)) )
+
+        # Voltage angle difference limit
+        model[:voltage_difference_limit][i] = JuMP.@constraint(model, branch["angmin"] <= va_fr - va_to <= branch["angmax"])
+
+        # Apparent power limit, from side and to side
+        model[:thermal_limit_fr][i] = JuMP.@constraint(model, p_fr^2 + q_fr^2 <= branch["rate_a"]^2)
+        model[:thermal_limit_to][i] = JuMP.@constraint(model, p_to^2 + q_to^2 <= branch["rate_a"]^2)
+    end
+
+    #
+    #   III. Objective
+    #
+    JuMP.@objective(model, Min, sum(
+        gen["cost"][1]*pg[i]^2 + gen["cost"][2]*pg[i] + gen["cost"][3]
+        for (i,gen) in ref[:gen]
+    ))
+
+    return model
+end
+
+"""
+    _extract_solution(model, data)
+Extract ACOPF solution from optimization model.
+The model must have been solved before.
+"""
+function _extract_solution(model::JuMP.Model, data::Dict{String,Any})
+    # Pre-process data
+    ref = PM.build_ref(data)[:it][:pm][:nw][0]
+    N = length(ref[:bus])
+    G = length(ref[:gen])
+    E = length(ref[:branch])
+
+    # Build the solution dictionary
+    res = Dict{String,Any}()
+    res["optimizer"] = JuMP.solver_name(model)
+    res["solve_time"] = JuMP.solve_time(model)
+    res["termination_status"] = JuMP.termination_status(model)
+    res["primal_status"] = JuMP.primal_status(model)
+    res["dual_status"] = JuMP.dual_status(model)
+
+    # TODO: extract primal solution
+    res["bus"] = Dict(
+                    "$bus" => Dict(
+                        "vm" => value(model[:vm][bus]),
+                        "va" => value(model[:va][bus])
+                        )
+                    for (bus, load) in ref[:bus_loads] if load != []
+                    )
+    res["branch"] = Dict(
+                    "$l" => Dict(
+                        "pf" => -value(model[:pf][(l,i,j)]),
+                        "pt" => value(model[:pf][(l,i,j)]),
+                        "qf" => value(model[:qf][(l,j,i)]),
+                        "qt" => -value(model[:qf][(l,j,i)])
+                        )
+                    for (l, i, j) in ref[:arcs]
+                    )
+    res["gen"] = Dict(
+                    "$gen" => Dict(
+                        "pg" => value(model[:pg][gen]),
+                        "qg" => value(model[:qg][gen])
+                        )
+                    for (gen, meta) in ref[:gen] if meta != []
+                    )
+    
+    # # TODO: extract dual solution
+
+    res["dual"] = Dict{String,Any}()
+    lam = res["dual"]
+
+    lam["power_balance"] = Dict(
+                    "$bus" => Dict(
+                        "active" => dual(model[:kirchhoff_active][bus]),
+                        "reactive" => dual(model[:kirchhoff_reactive][bus])
+                        )
+                    for (bus, load) in ref[:bus_loads] if load != []
+                    )
+    
+    lam["physics"] = Dict(
+                "$l" => Dict(
+                    "th_lim_to" => dual(model[:thermal_limit_to][l]),
+                    "th_lim_fr" => dual(model[:thermal_limit_fr][l]),
+                    "v_diff" => dual(model[:voltage_difference_limit][l]),
+                    "ohm_act_fr" => dual(model[:ohm_active_fr][l]),
+                    "ohm_act_to" => dual(model[:ohm_active_to][l]),
+                    "ohm_re_fr" => dual(model[:ohm_reactive_fr][l]),
+                    "ohm_re_to" => dual(model[:ohm_reactive_to][l])
+                    )
+                for (l, i, j) in ref[:arcs]
+                )  
+    
+    lam["slack_bus"] = dual(model[:slack_bus])
+    
+    return res
+end
