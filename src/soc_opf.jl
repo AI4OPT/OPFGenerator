a2f4430ad1e07fa3ee31ee937ac4f20ebb322158
Ivan <gqiu7@gatech.edu>
2023-07-06 18:41:58 -0400

    Finish soc_opf.jl

--- src/soc_opf.jl
+++ src/soc_opf.jl
@@ -3,158 +3,136 @@
 
 Build an SOC-OPF model.
 
-This implementation is based on the SOC-OPF formulation of PowerModelsAnnex.jl
-    https://github.com/lanl-ansi/PowerModelsAnnex.jl/blob/f303f3c3c61e2d1a050ee7651fa6e8abc4055b55/src/model/opf.jl
+This implementation is based on the SOC-OPF formulation of PMAnnex.jl
+    https://github.com/lanl-ansi/PMAnnex.jl/blob/f303f3c3c61e2d1a050ee7651fa6e8abc4055b55/src/model/opf.jl
 """
-function build_soc_opf(data::Dict{String,Any}, model=Model())
+function build_soc_opf(data::Dict{String,Any}, optimizer)
     @assert !haskey(data, "multinetwork")
     @assert !haskey(data, "conductors")
 
-    PowerModels.standardize_cost_terms!(data, order=2)
-    ref = PowerModels.build_ref(data)[:it][_PM.pm_it_sym][:nw][nw_id_default]
+    # Cleanup and pre-process data
+    PM.standardize_cost_terms!(data, order=2)
+    PM.calc_thermal_limits!(data)
+    # ref = PM.build_ref(data)[:it][_PM.pm_it_sym][:nw][nw_id_default]
+    ref = PM.build_ref(data)[:it][:pm][:nw][0]
+    
+    # Grab some data
+    N = length(ref[:bus])
+    G = length(ref[:gen])
+    # E = length(data["branch"])
+    E = length(ref[:branch])
+    # L = length(ref[:load])
+    bus_loads = [
+        [ref[:load][l] for l in ref[:bus_loads][i]]
+        for i in 1:N
+    ]
+    bus_shunts = [
+        [ref[:shunt][s] for s in ref[:bus_shunts][i]]
+        for i in 1:N
+    ]
 
-    @variable(model, ref[:bus][i]["vmin"]^2 <= w[i in keys(ref[:bus])] <= ref[:bus][i]["vmax"]^2, start=1.001)
+    model = JuMP.Model(optimizer)
 
-    wr_min, wr_max, wi_min, wi_max = PowerModels.ref_calc_voltage_product_bounds(ref[:buspairs])
+    #
+    #   I. Variables
+    #
+    # nodal voltage
+    JuMP.@variable(model, ref[:bus][i]["vmin"]^2 <= w[i in 1:N] <= ref[:bus][i]["vmax"]^2, start=1.001)
 
-    @variable(model, wr_min[bp] <= wr[bp in keys(ref[:buspairs])] <= wr_max[bp], start=1.0)
-    @variable(model, wi_min[bp] <= wi[bp in keys(ref[:buspairs])] <= wi_max[bp])
+    wr_min, wr_max, wi_min, wi_max = PM.ref_calc_voltage_product_bounds(ref[:buspairs])
 
-    @variable(model, ref[:gen][i]["pmin"] <= pg[i in keys(ref[:gen])] <= ref[:gen][i]["pmax"])
-    @variable(model, ref[:gen][i]["qmin"] <= qg[i in keys(ref[:gen])] <= ref[:gen][i]["qmax"])
+    JuMP.@variable(model, wr_min[bp] <= wr[bp in keys(ref[:buspairs])] <= wr_max[bp], start=1.0)
+    JuMP.@variable(model, wi_min[bp] <= wi[bp in keys(ref[:buspairs])] <= wi_max[bp])
+    # Active and reactive dispatch
+    JuMP.@variable(model, ref[:gen][g]["pmin"] <= pg[g in 1:G] <= ref[:gen][g]["pmax"])
+    JuMP.@variable(model, ref[:gen][g]["qmin"] <= qg[g in 1:G] <= ref[:gen][g]["qmax"])
+    # Bi-directional branch flows
+    JuMP.@variable(model, -ref[:branch][l]["rate_a"] <= pf[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+    JuMP.@variable(model, -ref[:branch][l]["rate_a"] <= qf[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
 
-    @variable(model, -Inf <= p[(l,i,j) in ref[:arcs]] <= Inf)
-    @variable(model, -Inf <= q[(l,i,j) in ref[:arcs]] <= Inf)
-    for arc in ref[:arcs]
-        (l,i,j) = arc
-        branch = ref[:branch][l]
-        if haskey(branch, "rate_a")
-            JuMP.set_lower_bound(p[arc], -branch["rate_a"])
-            JuMP.set_upper_bound(p[arc],  branch["rate_a"])
-            JuMP.set_lower_bound(q[arc], -branch["rate_a"])
-            JuMP.set_upper_bound(q[arc],  branch["rate_a"])
-        end
-    end
-    @variable(model, p_dc[a in ref[:arcs_dc]])
-    @variable(model, q_dc[a in ref[:arcs_dc]])
-
-    for (l,dcline) in ref[:dcline]
-        f_idx = (l, dcline["f_bus"], dcline["t_bus"])
-        t_idx = (l, dcline["t_bus"], dcline["f_bus"])
-
-        JuMP.set_lower_bound(p_dc[f_idx], dcline["pminf"])
-        JuMP.set_upper_bound(p_dc[f_idx], dcline["pmaxf"])
-        JuMP.set_lower_bound(q_dc[f_idx], dcline["qminf"])
-        JuMP.set_upper_bound(q_dc[f_idx], dcline["qmaxf"])
-
-        JuMP.set_lower_bound(p_dc[t_idx], dcline["pmint"])
-        JuMP.set_upper_bound(p_dc[t_idx], dcline["pmaxt"])
-        JuMP.set_lower_bound(q_dc[f_idx], dcline["qmint"])
-        JuMP.set_upper_bound(q_dc[f_idx], dcline["qmaxt"])
-    end
-
-    from_idx = Dict(arc[1] => arc for arc in ref[:arcs_from_dc])
-
-    @objective(model, Min,
-        sum(gen["cost"][1]*pg[i]^2 + gen["cost"][2]*pg[i] + gen["cost"][3] for (i,gen) in ref[:gen]) +
-        sum(dcline["cost"][1]*p_dc[from_idx[i]]^2 + dcline["cost"][2]*p_dc[from_idx[i]] + dcline["cost"][3] for (i,dcline) in ref[:dcline])
+    # 
+    #   II. Constraints
+    #
+    # Nodal power balance
+    JuMP.@constraint(model, 
+        kirchhoff_active[i in 1:N],
+        sum(pf[a] for a in ref[:bus_arcs][i]) ==
+        sum(pg[g] for g in ref[:bus_gens][i]) -
+        sum(load["pd"] for load in bus_loads[i]) -
+        sum(shunt["gs"] for shunt in bus_shunts[i])*w[i]
+    )
+    JuMP.@constraint(model,
+        kirchhoff_reactive[i in 1:N],
+        sum(qf[a] for a in ref[:bus_arcs][i]) ==
+        sum(qg[g] for g in ref[:bus_gens][i]) -
+        sum(load["qd"] for load in bus_loads[i]) +
+        sum(shunt["bs"] for shunt in bus_shunts[i])*w[i]
     )
 
-    for (bp, buspair) in ref[:buspairs]
-        i,j = bp
-
-        # Voltage Product Relaxation Lowerbound
-        @constraint(model, wr[(i,j)]^2 + wi[(i,j)]^2 <= w[i]*w[j])
-
-        vfub = buspair["vm_fr_max"]
-        vflb = buspair["vm_fr_min"]
-        vtub = buspair["vm_to_max"]
-        vtlb = buspair["vm_to_min"]
-        tdub = buspair["angmax"]
-        tdlb = buspair["angmin"]
-
-        phi = (tdub + tdlb)/2
-        d   = (tdub - tdlb)/2
-
-        sf = vflb + vfub
-        st = vtlb + vtub
-
-        # Voltage Product Relaxation Upperbound
-        @constraint(model, sf*st*(cos(phi)*wr[(i,j)] + sin(phi)*wi[(i,j)]) - vtub*cos(d)*st*w[i] - vfub*cos(d)*sf*w[j] >=  vfub*vtub*cos(d)*(vflb*vtlb - vfub*vtub))
-        @constraint(model, sf*st*(cos(phi)*wr[(i,j)] + sin(phi)*wi[(i,j)]) - vtlb*cos(d)*st*w[i] - vflb*cos(d)*sf*w[j] >= -vflb*vtlb*cos(d)*(vflb*vtlb - vfub*vtub))
-    end
-
-    for (i,bus) in ref[:bus]
-        bus_loads = [ref[:load][l] for l in ref[:bus_loads][i]]
-        bus_shunts = [ref[:shunt][s] for s in ref[:bus_shunts][i]]
-
-        # Bus KCL
-        @constraint(model,
-            sum(p[a] for a in ref[:bus_arcs][i]) +
-            sum(p_dc[a_dc] for a_dc in ref[:bus_arcs_dc][i]) ==
-            sum(pg[g] for g in ref[:bus_gens][i]) -
-            sum(load["pd"] for load in bus_loads) -
-            sum(shunt["gs"] for shunt in bus_shunts)*w[i]
-        )
-
-        @constraint(model,
-            sum(q[a] for a in ref[:bus_arcs][i]) +
-            sum(q_dc[a_dc] for a_dc in ref[:bus_arcs_dc][i]) ==
-            sum(qg[g] for g in ref[:bus_gens][i]) -
-            sum(load["qd"] for load in bus_loads) +
-            sum(shunt["bs"] for shunt in bus_shunts)*w[i]
-        )
-    end
-
+    # Branch power flow physics and limit constraints
+    # We pre-allocate constraint containers for simplicity
+    model[:thermal_limit_fr] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:thermal_limit_to] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:voltage_difference_limit_max] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:voltage_difference_limit_min] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_active_fr] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_active_to] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_reactive_fr] = Vector{JuMP.ConstraintRef}(undef, E)
+    model[:ohm_reactive_to] = Vector{JuMP.ConstraintRef}(undef, E)
     for (i,branch) in ref[:branch]
+        data["branch"]["$i"]["br_status"] == 0 && continue  # skip branch
+
         f_idx = (i, branch["f_bus"], branch["t_bus"])
         t_idx = (i, branch["t_bus"], branch["f_bus"])
         bp_idx = (branch["f_bus"], branch["t_bus"])
 
-        p_fr = p[f_idx]
-        q_fr = q[f_idx]
-        p_to = p[t_idx]
-        q_to = q[t_idx]
+        p_fr = pf[f_idx]
+        q_fr = qf[f_idx]
+        p_to = pf[t_idx]
+        q_to = qf[t_idx]
 
         w_fr = w[branch["f_bus"]]
         w_to = w[branch["t_bus"]]
         wr_br = wr[bp_idx]
         wi_br = wi[bp_idx]
 
-        # Line Flow
-        g, b = PowerModels.calc_branch_y(branch)
-        tr, ti = PowerModels.calc_branch_t(branch)
+        g, b = PM.calc_branch_y(branch)
+        tr, ti = PM.calc_branch_t(branch)
+        ttm = tr^2 + ti^2
         g_fr = branch["g_fr"]
         b_fr = branch["b_fr"]
         g_to = branch["g_to"]
         b_to = branch["b_to"]
-        tm = branch["tap"]^2
 
-        # AC Line Flow Constraints
-        @constraint(model, p_fr ==  (g+g_fr)/tm*w_fr + (-g*tr+b*ti)/tm*(wr_br) + (-b*tr-g*ti)/tm*(wi_br) )
-        @constraint(model, q_fr == -(b+b_fr)/tm*w_fr - (-b*tr-g*ti)/tm*(wr_br) + (-g*tr+b*ti)/tm*(wi_br) )
+        # From side of the branch flow
+        model[:ohm_active_fr][i] = JuMP.@NLconstraint(model, p_fr ==  (g+g_fr)/ttm*w_fr + (-g*tr+b*ti)/ttm*(wr_br) + (-b*tr-g*ti)/ttm*(wi_br) )
+        model[:ohm_reactive_fr][i] = JuMP.@NLconstraint(model, q_fr == -(b+b_fr)/ttm*w_fr - (-b*tr-g*ti)/ttm*(wr_br) + (-g*tr+b*ti)/ttm*(wi_br) )
 
-        @constraint(model, p_to ==  (g+g_to)*w_to + (-g*tr-b*ti)/tm*(wr_br) + (-b*tr+g*ti)/tm*(-wi_br) )
-        @constraint(model, q_to == -(b+b_to)*w_to - (-b*tr+g*ti)/tm*(wr_br) + (-g*tr-b*ti)/tm*(-wi_br) )
+        # To side of the branch flow
+        model[:ohm_active_to][i] = JuMP.@NLconstraint(model, p_to ==  (g+g_to)*w_to + (-g*tr-b*ti)/ttm*(wr_br) + (-b*tr+g*ti)/ttm*(-wi_br) )
+        model[:ohm_reactive_to][i] = JuMP.@NLconstraint(model, q_to == -(b+b_to)*w_to - (-b*tr+g*ti)/ttm*(wr_br) + (-g*tr-b*ti)/ttm*(-wi_br) )
 
-        # Phase Angle Difference Limit
-        @constraint(model, wi_br <= tan(branch["angmax"])*wr_br)
-        @constraint(model, wi_br >= tan(branch["angmin"])*wr_br)
+        # Voltage angle difference limit
+        model[:voltage_difference_limit_max][i] = JuMP.@constraint(model, wi_br <= tan(branch["angmax"])*wr_br)
+        model[:voltage_difference_limit_min][i] = JuMP.@constraint(model, wi_br >= tan(branch["angmin"])*wr_br)
 
-        # Apparent Power Limit, From and To
-        if haskey(branch, "rate_a")
-            @constraint(model, p[f_idx]^2 + q[f_idx]^2 <= branch["rate_a"]^2)
-            @constraint(model, p[t_idx]^2 + q[t_idx]^2 <= branch["rate_a"]^2)
-        end
+        # Apparent power limit, from side and to side
+        model[:thermal_limit_fr][i] = JuMP.@constraint(model, p_fr^2 + q_fr^2 <= branch["rate_a"]^2)
+        model[:thermal_limit_to][i] = JuMP.@constraint(model, p_to^2 + q_to^2 <= branch["rate_a"]^2)
     end
+    
+    # Voltage product relaxation lower bound
+    JuMP.@constraint(model, w_ij_lower_bound[(i, j) in keys(ref[:buspairs])], wr[(i,j)]^2 + wi[(i,j)]^2 <= w[i]*w[j])
 
-    for (i,dcline) in ref[:dcline]
-        # DC Line Flow Constraint
-        f_idx = (i, dcline["f_bus"], dcline["t_bus"])
-        t_idx = (i, dcline["t_bus"], dcline["f_bus"])
+    # In this formulation, we do not include the voltage product relaxation upper bound
 
-        @constraint(model, (1-dcline["loss1"])*p_dc[f_idx] + (p_dc[t_idx] - dcline["loss0"]) == 0)
-    end
+    #
+    #   III. Objective
+    #
+    JuMP.@objective(model, Min, sum(
+        gen["cost"][1]*pg[i]^2 + gen["cost"][2]*pg[i] + gen["cost"][3]
+        for (i,gen) in ref[:gen]
+    ))
 
     return model
 end
