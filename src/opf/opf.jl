ec89f6c33fe4cd921178c281d9dec5188c7dec27
mtanneau3 <mtanneau3@gatech.edu>
2023-07-27 19:02:25 +0000

    Streamline ACOPF modeling & result extraction

--- src/opf/opf.jl
+++ src/opf/opf.jl
@@ -1,3 +1,10 @@
+mutable struct OPFModel{OPF <: PM.AbstractPowerModel}
+    data::Dict{String,Any}
+    model::JuMP.Model
+end
+
+function build_opf(data, ::Type{<:PM.AbstractPowerModel}) end
+
 include("acopf.jl")      # ACCPPowerModel
 include("dcopf.jl")    # DCPPowerModel
 include("soc_opf.jl")  # SOCWRPowerModel
