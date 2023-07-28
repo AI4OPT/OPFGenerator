200dba6515cb07195d2a233ee1e69e737ed86142
mtanneau3 <mtanneau3@gatech.edu>
2023-07-28 21:29:27 +0000

    Update sampler script to match new config schema

--- exp/sampler.jl
+++ exp/sampler.jl
@@ -7,43 +7,46 @@ using PowerModels
 PowerModels.silence()
 using PGLib
 
-using OPFGenerator
 using JuMP
 using Ipopt
-using HSL
+using Mosek, MosekTools
+
+using HSL_jll
+const LIB_COINHSL = HSL_jll.libhsl_path
 
 using MathOptSymbolicAD
 
-const LIB_COINHSL = HSL.libcoinhsl
+using OPFGenerator
 
-function main(rng, opf_sampler, config)
+const NAME2OPTIMIZER = Dict(
+    "Ipopt" => Ipopt.Optimizer,
+    "Mosek" => Mosek.Optimizer,
+)
 
+function main(data, config)
     d = Dict{String,Any}()
     d["meta"] = deepcopy(config)
 
-    # Sample one ACOPF instance...
-    d["data"] = data_ = rand(rng, opf_sampler)
-    # ... solve it...
-    get(config["solver"], "name", "Ipopt") == "Ipopt" || error("Only Ipopt is supported as ACOPF solver.")
-    solver = optimizer_with_attributes(Ipopt.Optimizer,
-        "hsllib" => LIB_COINHSL,
-        "tol" => get(config["solver"], "tol", 1e-6),
-        "max_wall_time" => get(config["solver"], "max_wall_time", 3600.0),
-        "linear_solver" => get(config["solver"], "linear_solver", "mumps")
-    )
-    acopf = OPFGenerator.build_acopf(data_, solver)
-    set_silent(acopf)
-    # Symbolic AD is most useful for large systems
-    optimize!(acopf; _differentiation_backend = MathOptSymbolicAD.DefaultBackend())
-    # ... and export solution
-    acopf_res = OPFGenerator._extract_acopf_solution(acopf, data_)
-    d["acopf_res"] = acopf_res
+    # Keep track of initial data file
+    d["data"] = data
 
-    dcopf = OPFGenerator.build_dcopf(data_, solver)
-    set_silent(dcopf)
-    optimize!(dcopf; _differentiation_backend = MathOptSymbolicAD.DefaultBackend())
-    dcopf_res = OPFGenerator._extract_dcopf_solution(dcopf, data_)
-    d["dcopf_res"] = dcopf_res
+    # Solve all OPF formulations
+    for (opf_str, opf_solver) in config["OPF"]
+        OPF = OPFGenerator.OPF2TYPE[opf_str]
+        
+        solver = optimizer_with_attributes(NAME2OPTIMIZER[opf_solver],
+            config["solver"][opf_solver]...
+        )
+
+        opf = OPFGenerator.build_opf(OPF, data, solver)
+       
+        # Solve OPF model
+        set_silent(opf.model)
+        optimize!(opf.model; _differentiation_backend = MathOptSymbolicAD.DefaultBackend())
+
+        res = OPFGenerator.extract_result(opf)
+        d[opf_str] = res
+    end
 
     # Done
     return d
@@ -62,7 +65,8 @@ if abspath(PROGRAM_FILE) == @__FILE__
     # Dummy run (for pre-compilation)
     data0 = make_basic_network(pglib("14_ieee"))
     opf_sampler0 = OPFGenerator.SimpleOPFSampler(data0, config["sampler"])
-    main(StableRNG(1), opf_sampler0, config)
+    rand(StableRNG(1), opf_sampler0)
+    d0 = main(data0, config)
 
     # Load reference data and setup OPF sampler
     data = make_basic_network(pglib(config["ref"]))
@@ -72,7 +76,8 @@ if abspath(PROGRAM_FILE) == @__FILE__
     @info "Generating ACOPF & DCOPF instances for case $ref\nSeed range: [$smin, $smax]"
     for s in smin:smax
         rng = StableRNG(s)
-        tgen = @elapsed d = main(rng, opf_sampler, config)
+        data_ = rand(rng, opf_sampler)
+        tgen = @elapsed d = main(data_, config)
         d["meta"]["seed"] = s
         twrite = @elapsed save_json(joinpath(export_dir, config["ref"] * "_s$s.json.gz"), d)
         @info "Seed $s" tgen twrite
