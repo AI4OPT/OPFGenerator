d9fa2782bbff804cc5a7127c7e3c2ee53f9dac20
Mathieu Tanneau <9593025+mtanneau@users.noreply.github.com>
2023-07-27 20:18:42 -0400

    Add support for SOCWRConicPowerModel formulation (#30)
    * Add support for SOCWRConicPowerModel
    
    * Streamline unit tests
    
    * Fix typo in type parametrization
    
    ---------
    
    Co-authored-by: mtanneau3 <mtanneau3@gatech.edu>

--- test/opf/opf.jl
+++ test/opf/opf.jl
@@ -1,5 +1,60 @@
+using OPFGenerator: OPFModel
+
+function test_opf(::Type{OPF}, casename::String) where {OPF <: PM.AbstractPowerModel}
+    data = make_basic_network(pglib(casename))
+    return test_opf(OPF, data)
+end
+
+"""
+    test_opf_pm(OPF, data)
+
+Build & solve opf using OPFGenerator, and compare against PowerModels implementation.
+
+Returns the `OPFModel` struct, the result dictionary, and the PowerModels result dictionary.
+"""
+function test_opf(::Type{OPF}, data::Dict) where{OPF <: PM.AbstractPowerModel}
+    # Sanity checks
+    data["basic_network"] || error("Input data must be in basic format to test")
+    N = length(data["bus"])
+    E = length(data["branch"])
+    G = length(data["gen"])
+
+    # Solve OPF with PowerModels
+    solver = OPT_SOLVERS[OPF]
+    res_pm = PM.solve_opf(data, OPF, solver)
+
+    # Build and solve OPF with OPFGenerator
+    opf = OPFGenerator.build_opf(OPF, data, solver)
+    optimize!(opf.model)
+    res = OPFGenerator.extract_result(opf)
+    @test res["opf_model"] == string(OPF)
+    
+    # Check that problem was solved
+    @test res["termination_status"] ∈ [LOCALLY_SOLVED, OPTIMAL]
+    @test res["primal_status"] == FEASIBLE_POINT
+    @test res["dual_status"] == FEASIBLE_POINT
+    @test isapprox(res["objective"], res_pm["objective"], atol=1e-6, rtol=1e-6)
+
+    _test_opf_detailed(opf, res, res_pm)
+
+    return opf, res, res_pm
+end
+
+function _test_opf_detailed(opf, res, res_pm)
+    @test_broken false  # flag missing implementation
+end
+
+include("acp.jl")
+include("dcp.jl")
+include("socwr.jl")
+
+const OPF_MODELS  = [PM.ACPPowerModel, PM.SOCWRPowerModel, PM.SOCWRConicPowerModel, PM.DCPPowerModel]
+const PGLIB_CASES = ["14_ieee", "30_ieee", "57_ieee", "89_pegase", "118_ieee"]
+
 @testset "OPF" begin
-    include("acp.jl")
-    include("dcp.jl")
-    include("socwr.jl")
+    @testset "$(OPF)" for OPF in OPF_MODELS
+        @testset "$(casename)" for casename in PGLIB_CASES
+            test_opf(OPF, "pglib_opf_case$(casename)")
+        end
+    end
 end
--- test/opf/socwr.jl
+++ test/opf/socwr.jl
@@ -1,33 +1,12 @@
-_test_soc_opf(casename::String) = _test_soc_opf(PM.make_basic_network(pglib(casename)))
+const SOCWRPowerModel = Union{PM.SOCWRPowerModel,PM.SOCWRConicPowerModel}
 
-"""
-    _test_soc_opf(data)
-
-Solve ACOPF problem and compare to PowerModels' solution.
-"""
-function _test_soc_opf(data::Dict)
-    data["basic_network"] || error("Input data must be in basic format to test")
+function _test_opf_detailed(opf::OPFModel{OPF}, res::Dict, res_pm::Dict) where {OPF <: SOCWRPowerModel}
+    data = opf.data
     N = length(data["bus"])
     E = length(data["branch"])
     G = length(data["gen"])
-
-    solver = optimizer_with_attributes(Ipopt.Optimizer,
-        "print_level" => 0,
-    )
-    res_pm = solve_opf(data, SOCWRPowerModel, solver)
+    model = opf.model
     sol_pm = res_pm["solution"]
-
-    # build and solve SOC-OPF 
-    socp = OPFGenerator.build_opf(PM.SOCWRPowerModel, data, solver)
-    model = socp.model
-    optimize!(model)
-    res = OPFGenerator.extract_result(socp)
-    @test res["opf_model"] == "SOCWRPowerModel"
-    sol = res["solution"]
-
-    # Check that we get consistent results with PowerModels
-    @test res["termination_status"] == LOCALLY_SOLVED
-    @test isapprox(res["objective"], res_pm["objective"], atol=1e-6, rtol=1e-6)
 
     # Force PM solution into our model, and check that the solution is feasible
     # TODO: use JuMP.primal_feasibility_report instead
@@ -44,15 +23,8 @@ function _test_soc_opf(data::Dict)
     end
 
     optimize!(model)
-    @test termination_status(model) ∈ [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED]
+    @test termination_status(model) ∈ [OPTIMAL, LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED]
     @test primal_status(model) ∈ [FEASIBLE_POINT, NEARLY_FEASIBLE_POINT]
 
     return nothing
 end
-
-@testset "SOC-OPF" begin
-    @testset "$casename" for casename in ["14_ieee", "30_ieee", "57_ieee", "89_pegase", "118_ieee"]
-        _test_soc_opf("pglib_opf_case$(casename)")
-    end
-end
-
