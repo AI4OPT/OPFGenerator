c18913548a3aba3bcb05207259ca7ec926748117
Klamkin, Michael <mklamkin3@gatech.edu>
2024-02-22 11:03:03 -0500

    ask for sysimage with defaults (fixes #41)

--- slurm/template/submit.sh
+++ slurm/template/submit.sh
@@ -1,9 +1,43 @@
 #!/usr/bin/env bash
 
-sysimage_id=$(sbatch {{{:slurm_dir}}}/sysimage.sbatch | awk '{print $NF}')
-echo "Submitted sysimage job with id $sysimage_id"
+sysimage_exists="no"
+prompt_message="Do you want to create a sysimage at app/julia.so?"
+default_sysimage="yes"
+if [ -f "app/julia.so" ]; then
+    sysimage_exists="yes"
+    prompt_message="Do you want to re-create the sysimage at app/julia.so?"
+    default_sysimage="no"
+fi
 
-ref_id=$(sbatch --dependency=afterok:$sysimage_id {{{:slurm_dir}}}/ref.sbatch | awk '{print $NF}')
+ask_sysimage() {
+    while true; do
+        read -p "${prompt_message} (yes/no) [${default_sysimage}]: " yn
+        yn=${yn:-$default_sysimage} # Default value if input is empty
+        case $(echo "$yn" | tr '[:upper:]' '[:lower:]') in
+            'y' | 'yes' ) run_sysimage="yes"; break;;
+            'n' | 'no' ) run_sysimage="no"; break;;
+            * ) echo "Error: Unrecognized input. Please answer 'yes' or 'no'.";;
+        esac
+    done
+}
+
+ask_sysimage
+
+if [ "$run_sysimage" == "yes" ]; then
+    if [ "$sysimage_exists" == "yes" ]; then
+        echo "Re-creating sysimage..."
+    else
+        echo "Creating a sysimage..."
+    fi
+    sysimage_id=$(sbatch {{{:slurm_dir}}}/sysimage.sbatch | awk '{print $NF}')
+    echo "Submitted sysimage job with id $sysimage_id"
+    dependency_sysimage="--dependency=afterok:$sysimage_id"
+else
+    echo "Skipping sysimage job"
+    dependency_sysimage=""
+fi
+
+ref_id=$(sbatch $dependency_sysimage {{{:slurm_dir}}}/ref.sbatch | awk '{print $NF}')
 echo "Submitted ref job with id $ref_id"
 
 solve_id=$(sbatch --dependency=afterok:$ref_id {{{:slurm_dir}}}/sampler.sbatch | awk '{print $NF}')
