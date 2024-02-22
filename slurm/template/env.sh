771c9434e2a7370395c01b2beae44130d98bcd56
Klamkin, Michael <mklamkin3@gatech.edu>
2024-02-22 13:48:05 -0500

    add env file option (fixes #40)

---
+++ slurm/template/env.sh
@@ -0,0 +1,12 @@
+## add commands to this file that make gcc, parallel, and julia available.
+
+## if using lmod, grab the latest versions
+module load gcc parallel julia
+
+## if using bashrc, source it
+# source $HOME/.bashrc
+
+## if using manually installed software, make aliases
+# alias julia = "/full/path/to/julia/bin/julia"
+# alias gcc = "/full/path/to/gcc/bin/gcc"
+# alias parallel = "/full/path/to/parallel/bin/parallel"
