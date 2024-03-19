75046b9f8a03f0cf76a1133696b6b39cf6fe8ac0
mtanneau <mathieu.tanneau@gmail.com>
2024-03-19 18:02:06 -0400

    Update AC-OPF PowerModels tests

--- test/opf/acp.jl
+++ test/opf/acp.jl
@@ -1,20 +1,40 @@
-function _test_opf_detailed(opf::OPFGenerator.OPFModel{OPF}, res::Dict, res_pm::Dict) where {OPF <: PM.ACPPowerModel}
-    data = opf.data
+function test_opf_pm(::Type{PM.ACPPowerModel}, data::Dict)
+    OPF = PM.ACPPowerModel
+    # Sanity checks
+    data["basic_network"] || error("Input data must be in basic format to test")
     N = length(data["bus"])
     E = length(data["branch"])
     G = length(data["gen"])
-    model = opf.model
-    sol_pm = res_pm["solution"]
+
+    # Solve OPF with PowerModels
+    solver = OPT_SOLVERS[OPF]
+    res_pm = PM.solve_opf(data, OPF, solver)
+
+    # Build and solve OPF with OPFGenerator
+    solver = OPT_SOLVERS[OPF]
+    opf = OPFGenerator.build_opf(OPF, data, solver)
+    OPFGenerator.solve!(opf)
+    res = OPFGenerator.extract_result(opf)
+    @test res["opf_model"] == string(OPF)
+    
+    # Check that problem was solved
+    @test res["termination_status"] ∈ [LOCALLY_SOLVED, OPTIMAL]
+    @test res["primal_status"] == FEASIBLE_POINT
+    @test res["dual_status"] == FEASIBLE_POINT
+    # Check objective value against PowerModels
+    @test isapprox(res["objective"], res_pm["objective"], atol=1e-6, rtol=1e-6)
 
     # Force PM solution into our model, and check that the solution is feasible
     # TODO: use JuMP.primal_feasibility_report instead
     #    (would require extracting a variable => value Dict)
+    sol_pm = res_pm["solution"]
     var2val_pm = Dict(
         :pg => Float64[sol_pm["gen"]["$g"]["pg"] for g in 1:G],
         :qg => Float64[sol_pm["gen"]["$g"]["qg"] for g in 1:G],
         :va => Float64[sol_pm["bus"]["$i"]["va"] for i in 1:N],
         :vm => Float64[sol_pm["bus"]["$i"]["vm"] for i in 1:N],
     )
+    model = opf.model
     for varname in [:pg, :qg, :va, :vm]
         x = model[varname]
         v = var2val_pm[varname]
