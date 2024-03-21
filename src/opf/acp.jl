fafedb20cb045ebd47c4146ac22209ee421cc6ba
mtanneau <mathieu.tanneau@gmail.com>
2024-03-21 15:44:26 -0400

    Write ACOPF power balance with only constant load on RHS

--- src/opf/acp.jl
+++ src/opf/acp.jl
@@ -57,17 +57,19 @@ function build_opf(::Type{PM.ACPPowerModel}, data::Dict{String,Any}, optimizer;
     # Nodal power balance
     JuMP.@constraint(model, 
         kirchhoff_active[i in 1:N],
-        sum(pf[a] for a in ref[:bus_arcs][i]) ==
-        sum(pg[g] for g in ref[:bus_gens][i]) -
-        sum(load["pd"] for load in bus_loads[i]) -
-        sum(shunt["gs"] for shunt in bus_shunts[i])*vm[i]^2
+        sum(pg[g] for g in ref[:bus_gens][i]) 
+        - sum(pf[a] for a in ref[:bus_arcs][i])
+        - sum(shunt["gs"] for shunt in bus_shunts[i])*vm[i]^2
+        == 
+        sum(load["pd"] for load in bus_loads[i])
     )
     JuMP.@constraint(model,
         kirchhoff_reactive[i in 1:N],
-        sum(qf[a] for a in ref[:bus_arcs][i]) ==
-        sum(qg[g] for g in ref[:bus_gens][i]) -
-        sum(load["qd"] for load in bus_loads[i]) +
-        sum(shunt["bs"] for shunt in bus_shunts[i])*vm[i]^2
+        sum(qg[g] for g in ref[:bus_gens][i]) 
+        - sum(qf[a] for a in ref[:bus_arcs][i])
+        - sum(shunt["bs"] for shunt in bus_shunts[i])*vm[i]^2
+        ==
+        sum(load["qd"] for load in bus_loads[i])
     )
 
     # Branch power flow physics and limit constraints
