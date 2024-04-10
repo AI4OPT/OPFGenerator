046c510022286c5242c9178c1c375365d05ad8bb
mtanneau <mathieu.tanneau@gmail.com>
2024-04-10 17:45:03 -0400

    Simplify syntax of tensorize

--- src/process.jl
+++ src/process.jl
@@ -135,14 +135,18 @@ function convert_to_h5!(D::Dict)
 end
 
 """
-    tensorize(V::Vector)
+    tensorize(V)
 
 Concatenate elements of `V` into a higher-dimensional tensor.
 
-If `V` is a collection of scalars, the output is a `1xm` matrix,
-    whose i-th element is equal to `V[i]`.
+Similar to `Base.stack`, with one major difference: if `V` is a vector of scalars,
+    the result is a 2D array `M` whose last dimension is `length(V)`,
+    and such that `M[:, i] == V[i]`.
+
+This function is only defined for `Vector{T}` and `Vector{Array{T,N}}` inputs,
+    to avoid any unexpected behavior of `Base.stack`.
 """
-function tensorize(V::Vector)
+function tensorize(V::Vector{T}) where {T}
     # Check that all elements have same size
     length(V) > 0 || error("Trying to tensorize an empty collection")
 
