6006a765ccb27b5f10cceb84fd10350950ad49a9
Documenter.jl <documenter@juliadocs.github.io>
2024-06-21 20:32:19 +0000

    build based on d3f96d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OPFGenerator.jl","page":"Home","title":"OPFGenerator.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OPFGenerator.jl","category":"page"},{"location":"","page":"Home","title":"Home","text":"warning: Warning\nThis documentation is a work in progress. Please open an issue if content is missing / erroneous","category":"page"}]
+}
