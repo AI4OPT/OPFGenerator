2574f2acd8b47051e52ff8d717ad7b45248dad12
Michael Klamkin <mklamkin3@gatech.edu>
2024-09-20 14:57:14 -0400

    take branch vmin/vmax as input

--- src/opf/utils.jl
+++ src/opf/utils.jl
@@ -3,41 +3,44 @@ function voltage_phasor_bounds(data::OPFData)
     vmin, vmax = data.vmin, data.vmax
     dvamin, dvamax = data.dvamin, data.dvamax
     bus_fr, bus_to = data.bus_fr, data.bus_to
-    return voltage_phasor_bounds(E, vmin, vmax, dvamin, dvamax, bus_fr, bus_to)
+    vfmin = [vmin[bus_fr[e]] for e in 1:E]
+    vfmax = [vmax[bus_fr[e]] for e in 1:E]
+    vtmin = [vmin[bus_to[e]] for e in 1:E]
+    vtmax = [vmax[bus_to[e]] for e in 1:E]
+    return voltage_phasor_bounds(E, vfmin, vfmax, vtmin, vtmax, dvamin, dvamax)
 end
 
-function voltage_phasor_bounds(E, vmin, vmax, dvamin, dvamax, bus_fr, bus_to)
+function voltage_phasor_bounds(E, vfmin, vfmax, vtmin, vtmax, dvamin, dvamax)
     wr_min = zeros(Float64, E)
     wr_max = zeros(Float64, E)
     wi_min = zeros(Float64, E)
     wi_max = zeros(Float64, E)
-    for e in 1:E
-        i = bus_fr[e]
-        j = bus_to[e]
-        cosmin = cos(dvamin)
-        cosmax = cos(dvamax)
-        sinmin = sin(dvamin)
-        sinmax = sin(dvamax)
 
+    cosmin = cos.(dvamin)
+    cosmax = cos.(dvamax)
+    sinmin = sin.(dvamin)
+    sinmax = sin.(dvamax)
+
+    for e in 1:E
         wr_min[e] = (
-            dvamin >= 0 ? vmin[i] * vmin[j] * cosmax :
-            dvamax <= 0 ? vmin[i] * vmin[j] * cosmin :
-            vmin[i] * vmin[j] * min(cosmin, cosmax)
+            dvamin[e] >= 0 ? vfmin[e] * vtmin[e] * cosmax[e] :
+            dvamax[e] <= 0 ? vfmin[e] * vtmin[e] * cosmin[e] :
+            vfmin[e] * vtmin[e] * min(cosmin[e], cosmax[e])
         )
         wr_max[e] = (
-            dvamin >= 0 ? vmax[i] * vmax[j] * cosmin :
-            dvamax <= 0 ? vmax[i] * vmax[j] * cosmax :
-            vmax[i] * vmax[j] * 1.0
+            dvamin[e] >= 0 ? vfmax[e] * vtmax[e] * cosmin[e] :
+            dvamax[e] <= 0 ? vfmax[e] * vtmax[e] * cosmax[e] :
+            vfmax[e] * vtmax[e] * 1.0
         )
         wi_min[e] = (
-            dvamin >= 0 ? vmin[i] * vmin[j] * sinmin :
-            dvamax <= 0 ? vmax[i] * vmax[j] * sinmax :
-            vmax[i] * vmax[j] * sinmin
+            dvamin[e] >= 0 ? vfmin[e] * vtmin[e] * sinmin[e] :
+            dvamax[e] <= 0 ? vfmax[e] * vtmax[e] * sinmax[e] :
+            vfmax[e] * vtmax[e] * sinmin[e]
         )
         wi_max[e] = (
-            dvamin >= 0 ? vmax[i] * vmax[j] * sinmax :
-            dvamax <= 0 ? vmin[i] * vmin[j] * sinmax :
-            vmax[i] * vmax[j] * sinmax
+            dvamin[e] >= 0 ? vfmax[e] * vtmax[e] * sinmax[e] :
+            dvamax[e] <= 0 ? vfmin[e] * vtmin[e] * sinmax[e] :
+            vfmax[e] * vtmax[e] * sinmax[e]
         )
     end
 
