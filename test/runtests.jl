5417216ad08fface1c519e106cda1ca4bc39cc09
Michael Klamkin <mklamkin3@gatech.edu>
2024-09-20 20:29:21 -0400

    nice spacing

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,10 +22,10 @@ const CLRBL128_SOLVER = JuMP.optimizer_with_attributes(Clarabel.Optimizer{Float1
 )
 
 const OPT_SOLVERS = Dict(
-    OPFGenerator.ACOPF               => IPOPT_SOLVER,
-    OPFGenerator.SOCOPFQuad             => IPOPT_SOLVER,
-    OPFGenerator.SOCOPF        => CLRBL_SOLVER,
-    OPFGenerator.DCOPF               => CLRBL_SOLVER,
+    OPFGenerator.ACOPF             => IPOPT_SOLVER,
+    OPFGenerator.SOCOPFQuad        => IPOPT_SOLVER,
+    OPFGenerator.SOCOPF            => CLRBL_SOLVER,
+    OPFGenerator.DCOPF             => CLRBL_SOLVER,
     OPFGenerator.EconomicDispatch  => CLRBL_SOLVER,
 )
 
