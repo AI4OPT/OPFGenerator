96d308b64f4ce1791b1c5b24d3484f50dde7dcb4
Klamkin, Michael <mklamkin3@gatech.edu>
2024-09-23 16:02:36 -0400

    fix ptdf test

--- test/opf/ptdf.jl
+++ test/opf/ptdf.jl
@@ -3,15 +3,14 @@ using OPFGenerator: LazyPTDF, FullPTDF, OPFData, compute_flow!, ptdf_row
 function test_ptdf(network, data)
     N = length(network["bus"])
     E = length(network["branch"])
+    p = ones(N)
+    f = zeros(E)
 
     Φ_pm = PowerModels.calc_basic_ptdf_matrix(network)
     fpm = Φ_pm * p
 
     for PTDF in [FullPTDF, LazyPTDF]
         Φ = PTDF(data)
-
-        p = ones(N)
-        f = zeros(E)
         compute_flow!(f, p, Φ)
         @test isapprox(f, fpm; atol=1e-6)
 
