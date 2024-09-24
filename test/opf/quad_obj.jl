9b180fbd28bac2ba13a8adee6b607a0af832accb
Klamkin, Michael <mklamkin3@gatech.edu>
2024-09-24 13:04:42 -0400

    allow quadratic obj for ACOPF/SOCOPF

--- test/opf/quad_obj.jl
+++ test/opf/quad_obj.jl
@@ -1,12 +1,14 @@
 function test_quad_obj_warn(OPF)
-    data = make_basic_network(pglib("pglib_opf_case24_ieee_rts"))
+    if OPF ∉ [OPFGenerator.ACOPF, OPFGenerator.SOCOPF, OPFGenerator.SOCOPFQuad]
+        data = make_basic_network(pglib("pglib_opf_case24_ieee_rts"))
 
-    solver = OPT_SOLVERS[OPF]
+        solver = OPT_SOLVERS[OPF]
 
-    warn_msg = "Data pglib_opf_case24_ieee_rts has quadratic cost terms; those terms are being ignored"
-    opf = @test_logs (:warn, warn_msg) match_mode=:any OPFGenerator.build_opf(OPF, data, solver)
+        warn_msg = "Data pglib_opf_case24_ieee_rts has quadratic cost terms; those terms are being ignored"
+        opf = @test_logs (:warn, warn_msg) match_mode=:any OPFGenerator.build_opf(OPF, data, solver)
 
-    @test isa(objective_function(opf.model), JuMP.AffExpr)
+        @test isa(objective_function(opf.model), JuMP.AffExpr)
+    end
 
     return nothing
 end
