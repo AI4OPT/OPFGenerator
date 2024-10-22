57e2441da7557deb9505d53799ea9c1ebc83164e
mtanneau <mathieu.tanneau@gmail.com>
2024-10-22 10:53:18 -0400

    Fix accidental removing of all unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,3 +32,8 @@ const OPT_SOLVERS = Dict(
 
 const PGLIB_CASES = ["14_ieee", "30_ieee", "57_ieee", "89_pegase", "118_ieee"]
 
+include("io.jl")
+include("bridges.jl")
+include("sampler.jl")
+include("opf/opf.jl")
+include("postprocess.jl")
