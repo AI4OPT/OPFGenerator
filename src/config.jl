0cd216384babc6a1b5993c0df1ed3d61ad5e6fda
mtanneau <mathieu.tanneau@gmail.com>
2024-11-12 16:31:44 -0500

    Safeguard against empty-string case_name

--- src/config.jl
+++ src/config.jl
@@ -32,7 +32,7 @@ function _get_case_info(config)
         error("Invalid config: must provide either \"case_file\" or \"pglib_case\".")
     end
 
-    case_name = if haskey(config, "case_name")
+    case_name = if get(config, "case_name", "") != ""
         config["case_name"]
     else
         splitext(basename(case_file))[1]
