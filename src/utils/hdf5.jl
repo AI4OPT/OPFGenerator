ae5fc3813fb510dd63a72aee05595874b5b3523a
mtanneau <mathieu.tanneau@gmail.com>
2025-01-20 15:48:59 -0500

    Enable/disable conversion warning when saving HDF5 file

--- src/utils/hdf5.jl
+++ src/utils/hdf5.jl
@@ -19,12 +19,16 @@ const HDF5_SUPPORTED_NUMBER_TYPES = Union{
 load_h5 = HDF5.h5read
 
 """
-    save_h5(filename, D)
+    save_h5(filename, D; warn=true)
 
 Saves dictionary `D` to HDF5 file `filename`.
 
-All keys in `D` must be of `String` type, and it must be HDF5-compatible.
-Additional restrictions are enforced on the values of `D`, see below.
+# Arguments
+* `filename::AbstractString`: Path to the HDF5 file; must be a valid path.
+* `D`: Dictionary to save to the file. 
+    All keys in `D` must be of `String` type, and it must be HDF5-compatible.
+    Additional restrictions are enforced on the values of `D`, see below.
+* `warn::Bool=true`: Whether to raise a warning when converting numerical data.
 
 !!! warning
     Only the following types are supported:
@@ -34,12 +38,14 @@ Additional restrictions are enforced on the values of `D`, see below.
     * `Complex` versions of the above numeric types
     * Dense `Array`s of the the above scalar types
 
-    Numerical data of an unsupported type will be converted to `Float64` when possible.
-    An error will be thrown if the conversion is not possible.
+    Numerical data whose type is not listed above will be converted to `Float64`.
+    Set `warn=false` to disable such warnings.
+    An error will be thrown if this conversion is not possible 
+    
 """
-function save_h5(filename::AbstractString, D)
+function save_h5(filename::AbstractString, D; warn=true)
     h5open(filename, "w") do file
-        _save_h5(file, D)
+        _save_h5(file, D; warn)
     end
     return nothing
 end
@@ -52,7 +58,7 @@ _convert_to_h5_supported(::Type{T}) where{T<:Complex} = Complex{Float64}
 _convert_to_h5_supported(::Type{T}) where {T} = Any
 _convert_to_h5_supported(::Type{Array{T,N}}) where {T,N} = Array{_convert_to_h5_supported(T),N}
 
-function _save_h5(fid::HDF5.H5DataStore, D::Dict)
+function _save_h5(fid::HDF5.H5DataStore, D::Dict; warn=true)
     for (k, v) in D
         if isa(v, Dict)
             # TODO: check that `k` is a `String` and does not contain any `/` characters
@@ -62,7 +68,7 @@ function _save_h5(fid::HDF5.H5DataStore, D::Dict)
         else
             T = typeof(v)
             T5 = _convert_to_h5_supported(T)
-            # Error if we can't convert to a supported type
+            # We could not convert to a supported type --> throw an error
             ((T5 == Any) || ((T5 <: AbstractArray) && eltype(T) == Any)) && error(
                 """Unsupported data type for writing to an HDF5 group: \"$k\"::$(typeof(v)).
                 HDF5 only supports the following types:
@@ -76,8 +82,8 @@ function _save_h5(fid::HDF5.H5DataStore, D::Dict)
 
                 Consider either converting to a different type, or using a different file format.\n"""
             )
-            # Throw warning if conversion was inexact, unless it's an `AbstractString`
-            (T5 != T) && !(T <: AbstractString) && @warn(
+            # Raise a warning if we are converting from a non-HDF5-supported numerical type
+            warn && (T5 != T) && !(T <: AbstractString) && @warn(
                 """Unsupported data type for writing to an HDF5 group: \"$k\"::$(T).
                 This value was converted to $(T5), which may incur a loss of precision.""",
                 maxlog=10,
