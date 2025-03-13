f3e26ca471f6e52b4226a7fe73a338658a9ee188
mtanneau <mathieu.tanneau@gmail.com>
2025-03-13 14:09:21 -0400

    update repo URLs for page deploy

--- docs/make.jl
+++ docs/make.jl
@@ -37,6 +37,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo="github.com/AI4OPT/PGLearn.git",
+    repo="github.com/AI4OPT/PGLearn.jl.git",
     push_preview=true,
 )
